f8986e088f9b528406c29691b3c01a155390c3af
bada55fa7a55 <me@6d6178.com>
2026-03-20 11:24:03 +0100

    Add sitemap lastmod

--- vite.config.ts
+++ vite.config.ts
@@ -73,6 +73,23 @@ const siteMetaPlugin = (siteUrl: string, googleSiteVerification?: string): Plugi
 
     return { html: replaced, tags };
   },
+  generateBundle() {
+    this.emitFile({
+      type: 'asset',
+      fileName: 'sitemap.xml',
+      source: [
+        '<?xml version="1.0" encoding="UTF-8"?>',
+        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+        '  <url>',
+        `    <loc>${siteUrl}/</loc>`,
+        `    <lastmod>${buildTimestamp.split('T')[0]}</lastmod>`,
+        '    <changefreq>monthly</changefreq>',
+        '    <priority>1.0</priority>',
+        '  </url>',
+        '</urlset>',
+      ].join('\n'),
+    });
+  },
 });
 
 export default defineConfig(({ mode }) => {
